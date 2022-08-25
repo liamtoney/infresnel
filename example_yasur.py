1a87fa6e6de503c84d72e5d10d6375c367c0b2ad
Liam Toney <ldtoney@alaska.edu>
2022-08-24 16:34:19 -0800

    Swap legend

--- example_yasur.py
+++ example_yasur.py
@@ -66,7 +66,7 @@ ax.ticklabel_format(style='plain')
 ax.set_aspect('equal')
 ax.set_xlabel('UTM easting (m)')
 ax.set_ylabel('UTM northing (m)')
-ax.legend(loc='center left', frameon=False, bbox_to_anchor=(1.05, 0.5))
+# ax.legend(loc='center left', frameon=False, bbox_to_anchor=(1.05, 0.5))
 fig.tight_layout()
 fig.show()
 if SAVE_EXAMPLE_FIGURES:
@@ -84,7 +84,7 @@ for ax, var_name in zip(axes, ds.data_vars):
     ax.set_aspect('equal')
 axes[-1].set_xlabel('Horizontal distance (m)')
 axes[1].set_ylabel('Elevation (m)')
-# axes[1].legend(loc='center left', frameon=False, bbox_to_anchor=(1.05, 0.5))
+axes[1].legend(loc='center left', frameon=False, bbox_to_anchor=(1.05, 0.5))
 fig.tight_layout()
 fig.show()
 if SAVE_EXAMPLE_FIGURES:
