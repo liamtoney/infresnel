a66a627206f36978507e0e4854e9ff150104f9ea
Liam Toney <ldtoney@alaska.edu>
2022-11-23 12:59:24 -0900

    Intersphinx setup

--- doc/conf.py
+++ doc/conf.py
@@ -4,17 +4,30 @@
 # sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 project = 'infresnel'
-copyright = '2022, Liam Toney'
-author = 'Liam Toney'
 
-extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.apidoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.napoleon',
+    'sphinxcontrib.apidoc',
+]
 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 
+html_show_copyright = False
+
 apidoc_module_dir = '../infresnel'
 
 apidoc_toc_file = False  # Don't create a table of contents file (modules.rst)
 
+apidoc_separate_modules = True  # Give submodules their own page
+
 napoleon_numpy_docstring = False  # We are using Google docstring style
+
+intersphinx_mapping = {
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'python': ('https://docs.python.org/3/', None),
+    'xarray': ('https://docs.xarray.dev/en/stable/', None),
+}
