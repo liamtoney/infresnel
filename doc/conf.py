184b9118ba2f2c2e8300a31367c23a549e0c3b86
Liam Toney <ldtoney@alaska.edu>
2022-11-23 13:03:08 -0900

    Clean up conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,8 +1,3 @@
-# import sys
-# from pathlib import Path
-#
-# sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
-
 project = 'infresnel'
 
 extensions = [
@@ -12,20 +7,16 @@ extensions = [
     'sphinxcontrib.apidoc',
 ]
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 html_theme = 'sphinx_rtd_theme'
-
 html_show_copyright = False
 
 apidoc_module_dir = '../infresnel'
-
 apidoc_toc_file = False  # Don't create a table of contents file (modules.rst)
-
 apidoc_separate_modules = True  # Give submodules their own page
 
 napoleon_numpy_docstring = False  # We are using Google docstring style
 
+# These only need to cover the packages we reference from the docstrings
 intersphinx_mapping = {
     'numpy': ('https://numpy.org/doc/stable/', None),
     'python': ('https://docs.python.org/3/', None),
